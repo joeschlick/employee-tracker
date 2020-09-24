78e5d502c58450477ed2b594fac75c3d21454692
slowdive <schlickjoe@gmail.com>
2020-09-23 23:15:23 -0700

    added connection code for mysql db

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+const util = require("util");
+
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "1234",
+  database: "employees_db"
+});
+
+connection.connect();
+connection.query = util.promisify(connection.query)
+
+module.export = connection;
