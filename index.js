347a22bfd81a4224710788473e440c7e495b9598
slowdive <schlickjoe@gmail.com>
2020-09-23 23:20:47 -0700

    added psuedo code

---
+++ index.js
@@ -0,0 +1,7 @@
+//require db
+
+//require table thing
+//require inquirer
+
+// use async await for functions
+// one big function that runs your big inquirer as well as Switch statement to run the necessary functions
